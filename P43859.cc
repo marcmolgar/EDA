b2b88137209f2fefd1af276b5e52ff40a715db9d
Marc Molinuevo <marcmolgar95@gmail.com>
2019-11-28 10:46:52 +0100

    Weighted shortest path (1)

--- P43859.cc
+++ P43859.cc
@@ -1,66 +1,70 @@
-/* P13994: Weighted shortest path (2) */
+/* P43859: Weighted shortest path (1) */
 
 #include <iostream>
 #include <vector>
+#include <limits>
 #include <queue>
-#include <stack>
-#include <limits> //to allow definition below
 
 #define INFINIT numeric_limits<int>::max()
 
 using namespace std;
 
-typedef pair<int,int> arc;		//arc with cost
-typedef vector< vector<arc> > graph;	//graph with cost
+typedef pair<int,int> arc;              //arc with cost
+typedef vector< vector<arc> > Graph;    //Graph with cost
 
-void dijkstra(const graph &g, int x, vector<int> &d, vector<int> &p) {
+int n, m, x, y;
+Graph g;
+vector<int> d;
+/* vector<int> p */
+
+void read_graph() {
+    g = Graph(n);
+    for (int i = 0; i < m; ++i) {
+      int origin, destiny, cost;
+      cin >> origin >> destiny >> cost;
+      arc a;
+      a.first = cost;
+      a.second = destiny;
+      g[origin].push_back(a);
+    }
+}
+
+void dijkstra() {
   int n = g.size();
-  d = vector<int>(n, INFINIT); d[x] = 0;
-  p = vector<int>(n, -1);
-  vector<bool> s(n, false);
+  d = vector<int>(n, INFINIT);
+  d[x] = 0;
+  /* p = vector<int>(n, -1); */
+  vector<bool> visited(n, false);
   priority_queue< arc, vector<arc>, greater<arc> > q;
-  q.push(arc(0, x));
+  q.push(arc(0, x)); // push the initial vertex
   
   while (not q.empty()) {
-    int u = q.top().second; //vertex we depart from
+    int u = q.top().second; // vertex we depart from
     q.pop();
-    if (not s[u]) {
-      s[u] = true;
+    if (not visited[u]) {
+      visited[u] = true;
       for (int i = 0; i < g[u].size(); ++i) {
         int v = g[u][i].second;  //vertex we go to
         int c = g[u][i].first;   //cost of going to it
-        if (d[v] > d[u] + c) {
+        if (d[u] + c < d[v]) {
           d[v] = d[u] + c;
-          p[v] = u;
+          /* p[v] = u; */
           q.push(arc(d[v],v));
-} } } } }
+        }
+      }
+    }
+  }
+}
 
 int main() {
-  int n, m, x, y;
   while (cin >> n >> m) {
-    graph g(n);
-    arc a;
-    int k;
-    for (int i = 0; i < m; ++i) {
-      cin >> k;
-      cin >> a.second >> a.first;
-      g[k].push_back(a);
-    }
+    read_graph();
     cin >> x >> y;
-    vector<int> d;
-    vector<int> p;
-    dijkstra(g, x, d, p);
-    stack<int> s;
-    if (d[y] == INFINIT) cout << "no path from " << x << " to " << y << endl;
+    if (x == y) cout << 0 << endl;
     else {
-      int aux = y;
-      while (aux != -1) {
-        s.push(aux);
-        aux = p[aux];
-      }
-      while (s.size() > 1) {
-        cout << s.top() << " ";
-        s.pop();
-      }
-      cout << s.top() << endl;
-} } }
+      dijkstra();
+      if (d[y] == INFINIT) cout << "no path from " << x << " to " << y << endl;
+      else cout << d[y] << endl;
+    }
+  }
+}
