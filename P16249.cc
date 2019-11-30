9ba58552677899bd9538965ef44b577c5a4d6116
Marc Molinuevo <marc@MacBookBPdeMarc.home>
2019-11-30 09:40:06 +0100

    Task ordering

--- P16249.cc
+++ P16249.cc
@@ -7,54 +7,86 @@
 
 using namespace std;
 
-typedef vector< vector<int> > graph;
+typedef vector< vector<int> > Graph;
 
-void topological_sorting(graph &adj, vector<int> &indegree, vector<string> &s, map<string, int> &M) {
-	priority_queue< string, vector<string>, greater<string> > q;
-	for (int i = 0; i < adj.size(); ++i) {
-		if (indegree[i] == 0) q.push(s[i]);
+int n, m;
+Graph graph;
+vector<int> indegree;
+vector<string> task_names;
+map<string, int> task_index;
+list<string> result; // a dynamic structure needs to be used in order to satisfy the jutge efficiency requirements
+
+void read_tasks() {
+	for (int i = 0; i < n; ++i) {
+		string task;
+		cin >> task;
+		task_names[i] = task;
+		task_index[task] = i; //M.insert(make_pair(s[i]), i);
 	}
+}
 
-	list<string> l;
-	while (not q.empty()) {
-		int u = M[q.top()]; q.pop();
-		l.push_back(s[u]);
-		for (int i = 0; i < adj[u].size(); ++i) {
-			int k = adj[u][i];
-			if (--indegree[k] == 0) q.push(s[k]);
+void read_dependencies() {
+	cin >> m;
+
+	graph = Graph(n);
+	indegree = vector<int>(n, 0);
+
+	for (int i = 0; i < m; ++i) {
+		string task1, task2;
+		cin >> task1 >> task2;
+		graph[task_index[task1]].push_back(task_index[task2]);
+		++indegree[task_index[task2]];
+	}
+}
+
+void topological_sorting() {
+	priority_queue< string, vector<string>, greater<string> > q;
+
+	for (int i = 0; i < graph.size(); ++i) {
+		if (indegree[i] == 0) {
+			q.push(task_names[i]);
 		}
 	}
 
-	if (l.size() == s.size()) {
+	result = list<string>();
+	while (not q.empty()) {
+		int u = task_index[q.top()]; // u: vertex we depart from
+		q.pop();
+
+		result.push_back(task_names[u]);
+
+		for (int i = 0; i < graph[u].size(); ++i) {
+			int v = graph[u][i]; // v: vertex we want to go to
+			if (--indegree[v] == 0) {
+				q.push(task_names[v]);
+			}
+		}
+	}
+}
+
+void print_result() {
+	if (result.size() == task_names.size()) {
 		list<string>::iterator it;
-		for (it = l.begin(); it != l.end(); ++it) cout << *it;
-	} else cout << "NO VALID ORDERING";
+		for (it = result.begin(); it != result.end(); ++it) {
+			cout << *it;
+		}
+	} else {
+		cout << "NO VALID ORDERING";
+	}
 	cout << endl;
 }
 
 int main() {
-	int n;
 	while (cin >> n) {
-		map<string, int> M;
-		vector<string> s(n);
+		task_index = map<string, int>();
+		task_names = vector<string>(n);
 
-		for (int i = 0; i < n; ++i) {
-			cin >> s[i];
-			M[s[i]] = i; //M.insert(make_pair(s[i]), i);
-		}
+		read_tasks();
 
-		int m;
-		cin >> m;
+		read_dependencies();
 
-		graph adj(n);
-		vector<int> indegree(n, 0);
+		topological_sorting();
 
-		for (int i = 0; i < m; ++i) {
-			string s1, s2;
-			cin >> s1 >> s2;
-			adj[M[s1]].push_back(M[s2]);
-			++indegree[M[s2]];
-		}
-		topological_sorting(adj, indegree, s, M);
+		print_result();
 	}
 }
